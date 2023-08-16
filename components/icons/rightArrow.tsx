4239b2e1e0895efac4f18d2fc2b7c945a0ccb918
Ash Uppal <ashuppal@gmail.com>
2023-08-16 00:06:00 -0700

    page complete

---
+++ components/icons/rightArrow.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+export const RightArrow = ({
+  className,
+  ...props
+}: React.SVGProps<SVGSVGElement>) => {
+  return (
+
+<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="Component 985">
+<path id="Icon" d="M7.5 14.9998L12.5 9.99982L7.5 4.99982" stroke="#667085" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+</svg>
+
+
+  );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- components/ui/securityTextArea.tsx
+++ components/ui/securityTextArea.tsx
@@ -2,6 +2,10 @@
 
 import React from "react";
 import { BlueLock } from "./blueLock";
+import { Fingerprint } from "../icons/fingerprint";
+import { RightArrow } from "../icons/rightArrow";
+
+
 export const SecurityTextArea = () => {
   return (
     <div className="flex flex-col items-start space-y-3 w-[550px] h-[76px] self-stretch p-3">
@@ -38,29 +42,59 @@ export const SecurityTextArea = () => {
         border: '1px solid var(--grey-200, #EAECF0)',
         background: 'var(--base-white, #FFF)',
         boxShadow: '0px 1px 5px 0px rgba(16, 24, 40, 0.02)'
-      }} className="flex p-3 items-center rounded-lg bg-white">
-        <div style={{
-          width: '44px',
-          height: '44px',
-          border: '1.5px solid var(--gray-200, #EAECF0)'
-        }} className="flex justify-center items-center gap-2.5 rounded-lg">
-          <BlueLock />
+      }} className="flex justify-between p-3 items-center rounded-lg bg-white">
+        <div className="flex items-center">
+          <div style={{
+            width: '44px',
+            height: '44px',
+            border: '1.5px solid var(--gray-200, #EAECF0)'
+          }} className="flex justify-center items-center gap-2.5 rounded-lg">
+            <BlueLock />
+          </div>
+          <span
+            style={{
+              color: 'var(--grey-900, #101828)',
+              fontSize: '14px',
+              fontFamily: 'Inter',
+              fontWeight: 500
+            }}
+            className="ml-2">
+            Update Password
+          </span>
         </div>
 
-        <span
-          style={{
-            color: 'var(--grey-900, #101828)',
-            fontSize: '14px',
-            fontFamily: 'Inter',
-            fontWeight: 500
-          }}
-          className="ml-2">
-          Update Password
-        </span>
-
+        <RightArrow />
       </div>
 
+      <div style={{
+        width: '550px',
+        border: '1px solid var(--grey-200, #EAECF0)',
+        background: 'var(--base-white, #FFF)',
+        boxShadow: '0px 1px 5px 0px rgba(16, 24, 40, 0.02)'
+      }} className="flex justify-between p-3 items-center rounded-lg bg-white">
+        <div className="flex items-center">
+          <div style={{
+            width: '44px',
+            height: '44px',
+            border: '1.5px solid var(--gray-200, #EAECF0)'
+          }} className="flex justify-center items-center gap-2.5 rounded-lg">
+            <Fingerprint />
+          </div>
+          <span
+            style={{
+              color: 'var(--grey-900, #101828)',
+              fontSize: '14px',
+              fontFamily: 'Inter',
+              fontWeight: 500
+            }}
+            className="ml-2">
+             Fingerprint Login
+          </span>
+        </div>
 
+        <RightArrow />
+      </div>
+     
     </div>
   )
 }
